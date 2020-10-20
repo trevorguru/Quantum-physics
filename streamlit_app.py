314a51298095cc93a6e06f4931fb0b5805a55d95
Trevor David Rhone <tnr2103@caa.columbia.edu>
2020-10-20 00:58:37 -0400

    updated streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ if st.checkbox('checkbox'):
 
 st.write(st.__version__)
     
-df = pd.DataFrame({'ts': ['2020-04-14 01:00:00']})
-df['ts2'] = pd.to_datetime(df['ts'])
-st.write(df)
-st.write('ts2 is actually:', df.loc[0, 'ts2'])
-st.write(tzlocal.get_localzone())
+df = pd.DataFrame()
+df.from_csv("exam1stats.csv")
+#st.write(df)
+st.write('exam scores actually')
+#st.write(tzlocal.get_localzone())
 
 st.title("Test App!!!")
 
-st.write("don't mess with the best")
+#st.write("don't mess with the best")
 
 file_obj = st.sidebar.file_uploader('Choose an image:', ('jpg', 'jpeg'))
 
@@ -24,15 +24,13 @@ value = st.slider("Pick a number", 0, 10, 3)
 st.write(value)
 
 input = st.text_input("Tell me something", "Cantami o Diva")
-
-with open("temp_file.txt", "a") as f:
-  f.write(f"{input}\n")
   
-with open("temp_file.txt", "r") as f:
-  st.write(f.read())
+#with open("temp_file.txt", "r") as f:
+#  st.write(f.read())
+#st.write("Streamlit is fabulous")
 
-st.write("Streamlit is fabulous")
+st.line_chart(df[-1])
 
-st.write("Hello Corey, this is the demo!")
-st.balloons()
-print("this is a log line")
+#st.write("Hello Corey, this is the demo!")
+#st.balloons()
+#print("this is a log line")
